e9a4f7f3e2f463453392e7944d9758455ca95f4d
i-qof <muhammadfadly.mfd@gmail.com>
2024-07-22 19:12:21 +0700

    templete

---
+++ templete/head.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr" data-bs-theme="light" data-color-theme="Blue_Theme">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Favicon icon-->
+    <link rel="shortcut icon" type="image/png" href="assets_modern/images/logos/favicon.png" />
+
+    <!-- Core Css -->
+    <link rel="stylesheet" href="assets_modern/css/styles.css" />
+
+    <title>Sensor</title>
+</head>
+
+<body>
+    <!-- Preloader -->
+    <!-- <div class="preloader">
+        <img src="assets_modern/images/logos/favicon.png" alt="loader" class="lds-ripple img-fluid" />
+    </div> -->
