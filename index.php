82ee8a08f66f1597a7ea71369afaf5dc91878bff
i-qof <muhammadfadly.mfd@gmail.com>
2024-07-22 20:10:25 +0700

    login

--- index.php
+++ index.php
@@ -52,11 +52,11 @@
                                 <form name="login" action="otentikasi.php" role="form" method="post">
                                     <div class="mb-3">
                                         <label for="exampleInputEmail1" class="form-label">Username</label>
-                                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                        <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp">
                                     </div>
                                     <div class="mb-4">
                                         <label for="exampleInputPassword1" class="form-label">Password</label>
-                                        <input type="password" class="form-control" id="exampleInputPassword1">
+                                        <input type="password" class="form-control" id="password" name="password">
                                     </div>
 
                                     <button type="submit" class="btn btn-primary w-100 py-8 mb-4 rounded-2">Sign In</button>
