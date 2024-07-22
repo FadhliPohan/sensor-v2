93095e79301db8d42f912406314cd1ddd72140c0
i-qof <muhammadfadly.mfd@gmail.com>
2024-07-22 18:57:22 +0700

    add asset

---
+++ zzz.php
@@ -0,0 +1,13 @@
+<?php
+// include "head.php";
+include "templete/head.php";
+include "templete/sidebar.php";
+?>
+
+<h1>haya</h1>
+<?php
+include "templete/script.php";
+?>
+<?php
+include "templete/footer.php";
+?>
