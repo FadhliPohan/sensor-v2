805bad45f5be8e63755fe1e9d013007f53e63153
i-qof <muhammadfadly.mfd@gmail.com>
2024-07-22 20:06:03 +0700

    foot

--- templete/footer.php
+++ templete/footer.php
@@ -315,14 +315,14 @@
 </div>
 <div class="dark-transparent sidebartoggler"></div>
 <!-- Import Js Files -->
-<script src="../assets_modern/libs/jquery/dist/jquery.min.js"></script>
-<script src="../assets_modern/js/app.min.js"></script>
-<script src="../assets_modern/js/app.init.js"></script>
-<script src="../assets_modern/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<script src="../assets_modern/libs/simplebar/dist/simplebar.min.js"></script>
+<script src="assets_modern/libs/jquery/dist/jquery.min.js"></script>
+<script src="assets_modern/js/app.min.js"></script>
+<script src="assets_modern/js/app.init.js"></script>
+<script src="assets_modern/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="assets_modern/libs/simplebar/dist/simplebar.min.js"></script>
 
-<script src="../assets_modern/js/sidebarmenu.js"></script>
-<script src="../assets_modern/js/theme.js"></script>
+<script src="assets_modern/js/sidebarmenu.js"></script>
+<script src="assets_modern/js/theme.js"></script>
 
 </body>
 
